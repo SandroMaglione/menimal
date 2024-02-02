f732c0194e5d2c88971e34221187a2534901cd96
SandroMaglione <lass.maglio@gmail.com>
2024-02-02 05:23:19 +0100

    file system read local files

--- src/bin.ts
+++ src/bin.ts
@@ -1,5 +1,29 @@
+import * as Fs from "@effect/platform-node/FileSystem";
 import { Console, Effect } from "effect";
 
-const program = Console.log("Hello, World!");
+const program = Effect.gen(function* (_) {
+  const fs = yield* _(Fs.FileSystem);
+  const fileNames = yield* _(fs.readDirectory(`./pages`));
+  yield* _(Console.log("Files in 'pages':", fileNames));
 
-Effect.runSync(program);
+  const files = yield* _(
+    Effect.all(
+      fileNames.map((fileName) => fs.readFileString(`./pages/${fileName}`)),
+      { concurrency: "unbounded" } // TODO
+    )
+  );
+  yield* _(Console.log("File content:", files));
+
+  return;
+});
+
+const runnable = program.pipe(Effect.provide(Fs.layer));
+
+const main = runnable.pipe(
+  Effect.catchTags({
+    BadArgument: (error) => Console.log("Arg error", error),
+    SystemError: (error) => Console.log("System error", error),
+  })
+);
+
+Effect.runPromise(main);
